408685024ddcad1deac8f1e5e04b879b8394f832
RusH <rus_halikov@mail.ru>
2017-02-06 00:02:09 +0200

    recursion calculator refactoring

--- src/main/java/com/rush/rainyhills/core/RecursionCalculator.java
+++ src/main/java/com/rush/rainyhills/core/RecursionCalculator.java
@@ -8,73 +8,67 @@ package com.rush.rainyhills.core;
 public class RecursionCalculator implements IVolumeCalculator {
     @Override
     public int calculate(int[] emptyHills) {
-        return calculate(emptyHills, 0);
+        return calculate(0, maxHillIndex(1, emptyHills), emptyHills);
     }
 
     @Override
     public int[] calculateHills(int[] emptyHills) {
         int[] fullHills = new int[emptyHills.length];
         System.arraycopy(emptyHills, 0, fullHills, 0, emptyHills.length);
-        calculateHills(emptyHills, fullHills, 0);
+        calculateHills(0, maxHillIndex(1, emptyHills), emptyHills, fullHills);
         return fullHills;
     }
 
-    private int calculate(int[] hills, int startIndex) {
+    private int calculate(int startIndex, int endIndex, int[] emptyHills) {
         int volume = 0;
-        if (startIndex < hills.length) {
-            int boardHill = hills[startIndex];
+        if (endIndex < emptyHills.length) {
+            int blockHill = Math.min(emptyHills[startIndex], emptyHills[endIndex]);
 
-            startIndex++;
-            int endIndex = findMaxIndex(hills, startIndex);
-            if (endIndex < hills.length) {
-                boardHill = boardHill < hills[endIndex] ? boardHill : hills[endIndex];
-                for (int i = startIndex; i < endIndex; i++) {
-                    if (hills[i] > boardHill) {
-                        boardHill = hills[i];
-                    } else {
-                        volume += boardHill - hills[i];
-                    }
+            startIndex = startIndex + 1;
+            for (int i = startIndex; i < endIndex; i++) {
+                if (emptyHills[i] > blockHill) {
+                    blockHill = emptyHills[i];
+                } else {
+                    volume += blockHill - emptyHills[i];
                 }
-
-                startIndex = endIndex;
-                volume += calculate(hills, startIndex);
             }
+
+            startIndex = endIndex;
+            endIndex = maxHillIndex(startIndex + 1, emptyHills);
+            volume += calculate(startIndex, endIndex, emptyHills);
         }
         return volume;
     }
 
-    private int[] calculateHills(int[] emptyHills, int[] fullHills, int startIndex) {
-        if (startIndex < emptyHills.length) {
-            int maxHill = emptyHills[startIndex];
+    private int[] calculateHills(int startIndex, int endIndex, int[] emptyHills, int[] fullHills) {
+        if (endIndex < emptyHills.length) {
+            int blockHill = Math.min(emptyHills[startIndex], emptyHills[endIndex]);
 
-            startIndex++;
-            int endIndex = findMaxIndex(emptyHills, startIndex);
-            if (endIndex < emptyHills.length) {
-                maxHill = maxHill < emptyHills[endIndex] ? maxHill : emptyHills[endIndex];
-                for (int i = startIndex; i < endIndex; i++) {
-                    if (emptyHills[i] > maxHill) {
-                        maxHill = emptyHills[i];
-                    } else {
-                        fullHills[i] = maxHill;
-                    }
+            startIndex = startIndex + 1;
+            for (int i = startIndex; i < endIndex; i++) {
+                if (emptyHills[i] > blockHill) {
+                    blockHill = emptyHills[i];
+                } else {
+                    fullHills[i] = blockHill;
                 }
-
-                startIndex = endIndex;
-                calculateHills(emptyHills, fullHills, startIndex);
             }
+
+            startIndex = endIndex;
+            endIndex = maxHillIndex(startIndex + 1, emptyHills);
+            calculateHills(startIndex, endIndex, emptyHills, fullHills);
         }
         return fullHills;
     }
 
-    private int findMaxIndex(int[] emptyHills, int startIndex) {
+    private int maxHillIndex(int startIndex, int[] emptyHills) {
         int maxHill = 0;
-        int maxIndex = startIndex;
+        int maxHillIndex = startIndex;
         for (int i = startIndex; i < emptyHills.length; i++) {
             if (emptyHills[i] > maxHill) {
                 maxHill = emptyHills[i];
-                maxIndex = i;
+                maxHillIndex = i;
             }
         }
-        return maxIndex;
+        return maxHillIndex;
     }
 }
